d50f89db528591fd594e283ef76617e117dc0851
eltonsarmanho <s0@19t2&>
2022-06-20 20:07:31 -0300

    done

--- EDA/AnaliseCensoSaeb.py
+++ EDA/AnaliseCensoSaeb.py
@@ -6,11 +6,13 @@ import pandas as pd
 import os
 from factor_analyzer import FactorAnalyzer
 import numpy as np
+import matplotlib.pyplot as plt
 
 def load():
     try:
         dataset = pd.read_csv('../Dataset/inep_sabe_merge_2019_new.csv', delimiter='\t')
         print(dataset.shape)
+        print(dataset.columns)
 
         return dataset;
 
@@ -35,13 +37,52 @@ def checkFeasibility(dataset):
     cols_to_drop = nunique[nunique == 1].index
     dataset_reduce = dataset_reduce.drop(cols_to_drop, axis=1)
     print("Dimensionality reduced from {} to {}.".format(dataframe.shape, dataset_reduce.shape))
-
+    print(dataset.columns)
 
     chi_square_value, p_value = calculate_bartlett_sphericity(dataset_reduce)
     print(chi_square_value, p_value);
 
     kmo_all, kmo_model = calculate_kmo(dataset_reduce)
     print('kmo: ', kmo_model)
+
+    # Criamos objeto factor_analysis, sem rotação e usando 5 fatores (tentativamente)
+    fa = FactorAnalyzer(40, rotation=None)
+
+    # Aplicamos o método fit (ajuste) desse objeto no dataframe
+    fa.fit(dataset_reduce)
+
+    # Depois desse ajuste podemos coletar os autovetores e autovalores
+    ev, v = fa.get_eigenvalues()
+    print('São ' + str(len(ev)) + ' autovalores:\n', ev)
+
+    # Create scree plot using matplotlib
+    plt.scatter(range(1, dataset_reduce.shape[1] + 1), ev)
+    plt.plot(range(1, dataset_reduce.shape[1] + 1), ev)
+    plt.title('Scree Plot')
+    plt.xlabel('Factors')
+    plt.ylabel('Eigenvalue')
+    plt.grid()
+    plt.show()
+
+    # 6 fatores
+    fa = FactorAnalyzer(30, rotation="varimax")
+
+    # o objeto tem o método fit para análise do dataframe
+    fa.fit(dataset_reduce)
+
+    # Desse extraimos as cargas fatoriais (factor loadings)
+    # Observe que fa.loadings_ é um numpy.array com shape (25,6). Usamos o método
+    # do pandas pd.DataFrame.from_records para convertê-lo em um dataframe
+    factorLoadings = pd.DataFrame.from_records(fa.loadings_)
+
+    # Para ver a dataframe gerado:
+    #factorLoadings.head(4)
+    # Substitue as linhas pelo nomes dos itens
+    factorLoadings.index = dataset_reduce.columns
+
+    plt.figure(figsize=(8, 6))
+    sns.set(font_scale=.9)
+    sns.heatmap(factorLoadings, linewidths=1, linecolor='#ffffff', cmap="YlGnBu", xticklabels=1, yticklabels=1)
 
 if __name__ == '__main__':
     dataframe = load()
