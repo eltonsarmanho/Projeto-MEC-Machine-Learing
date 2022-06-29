e7e6e20fe47017b2a6758a66f46e4c58fccc1570
eltonsarmanho <s0@19t2&>
2022-06-29 11:39:10 -0300

    Done..

---
+++ EDA/ScriptMergeMatriculaRegiao.py
@@ -0,0 +1,49 @@
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import cm
+from matplotlib.colors import ListedColormap
+
+pd.set_option("display.max.columns", None)
+import sys
+import matplotlib.pyplot as plt
+import time
+from dask import dataframe as dd
+import glob
+import os
+from tqdm import tqdm
+import itertools
+import csv
+import os
+import re
+
+def loadMatriculaWithDask(url, separate=None,):
+    start = time.time()
+    if(separate != None):
+        dataframe = dd.read_csv(url,dtype='object' ,sep=separate )
+    else: dataframe = dd.read_csv(url,dtype='object',blocksize="10MB" )
+
+    end = time.time()
+    print("Read csv without chunks: ", (end - start), "sec")
+    print(dataframe.shape)
+    print(dataframe.columns)
+
+def concatCSVWithDask(filefind,file):
+    # list of merged files returned
+    files = os.path.join("../Dataset/2017", filefind + '*.csv')
+
+    all_files = glob.glob(files)
+    print(all_files)
+    out_file = '../Dataset/2017/' + file + '.csv';
+    with open(out_file, 'w') as outfile:
+        for i, filename in enumerate(all_files):
+            print(i, filename)
+            with open(filename, 'r') as infile:
+                for rownum, line in enumerate(infile):
+                    if (i != 0) and (rownum == 0):  # Only write header once
+                        continue
+                    outfile.write(line)
+if __name__ == '__main__':
+    #loadMatriculaWithDask('../Dataset/2017/matricula_reduzido_nordeste.csv',)
+    concatCSVWithDask('matricula_reduzido_','matricula_reduzido_all_2017')
