4c76b43d59f225c18f20998cc10a2b91abe19672
Giovani Portelinha Maia <giovanipmaia@gmail.com>
2022-12-06 19:02:15 -0300

    Correção das urls para a cloud.

---
+++ docker/urls.py
@@ -0,0 +1,53 @@
+"""machinelearning URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from prediction import views
+from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Machine Learning API",
+        default_version='v1',
+        description="Teste"
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
+router = routers.DefaultRouter()
+router.register('dimensoes', views.DimensaoViewSet, basename='dimensoes')
+router.register('fatores', views.FatorViewSet, basename='fatores')
+router.register('dimensoes_est', views.DimensaoESTViewSet, basename='dimensoes_est')
+router.register('fatore_est', views.FatorESTViewSet, basename='fatores_est')
+
+urlpatterns = [
+    path('apa-machine-learning/admin/', admin.site.urls),
+    path('apa-machine-learning/api/', include(router.urls)),
+    path('apa-machine-learning/processamento_from_json/', views.ProcessamentoFromJson.as_view()),
+    path('apa-machine-learning/', views.InitialView.as_view(), name='home'),
+    path('apa-machine-learning/sapdimensoes/', views.SapDimensoesView.as_view(), name='sapdimensoes'),
+    path('apa-machine-learning/sapfatores/', views.SapFatoresView.as_view(), name='sapfatores'),
+
+    path('apa-machine-learning/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('apa-machine-learning/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
