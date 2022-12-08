50bfcc3109afc65be995f378e679bef6af0bd31a
emanuelqueiroga <emanuelmqueiroga@gmail.com>
2022-12-08 19:08:58 -0300

    teste 3

--- prediction/views.py
+++ prediction/views.py
@@ -237,7 +237,7 @@ class GeralView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(GeralView, self).get_context_data(**kwargs)
         from prediction.graphics import texto_sap_quant_est_esc
-        #from prediction.graphics import texto_apa_quant_est_esc
+        from prediction.graphics import texto_apa_quant_est_esc
         #from prediction.graphics import table_apa_ciclo
         #from prediction.graphics import media_dimensoes
         #import time
@@ -246,7 +246,7 @@ class GeralView(TemplateView):
 
         context['titulo'] = 'Visão geral dos sistemas atuais'
         context['text1'] = texto_sap_quant_est_esc()
-        #context['text2'] = texto_apa_quant_est_esc()
+        context['text2'] = texto_apa_quant_est_esc()
 
         #context['table1'] = table_apa_ciclo().to_html()
         #context['table2'] = media_dimensoes().to_html()
